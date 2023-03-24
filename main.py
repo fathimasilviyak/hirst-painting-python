1e1bfc60e183f52fd6e55774d66ba168de75ded4
fathimasilviyak <silviyakvm@gmail.com>
2023-03-24 11:34:57 +0530

    extract colors from a hirst image and create a list of rgb colors

---
+++ main.py
@@ -0,0 +1,15 @@
+# import colorgram
+# # Extract 30 colors from an image.
+# rgb_colors = []
+# colors = colorgram.extract('image.jpg', 30)
+#
+# for color in colors:
+#     r = color.rgb.r
+#     g = color.rgb.g
+#     b = color.rgb.b
+#     new_color = (r, g, b)
+#     rgb_colors.append(new_color)
+# print(rgb_colors)
+
+# rgb_colors:
+colors = [(22, 30, 46), (140, 173, 200), (36, 105, 148), (205, 160, 112), (226, 210, 99), (140, 29, 62), (169, 51, 83), (208, 74, 101), (192, 139, 172), (16, 163, 191), (64, 167, 115), (136, 88, 73), (117, 181, 115), (190, 182, 43), (223, 81, 47), (27, 61, 117), (172, 209, 179), (72, 34, 67), (51, 141, 110), (9, 90, 108), (239, 204, 5), (217, 176, 195), (115, 36, 34), (232, 169, 162), (146, 208, 226), (186, 186, 212)]
